d1548f238d5ef91e3a1ba004957e421c5ead377f
saravanand <saravanan.d@techcello.com>
2016-12-11 20:54:57 +0530

    initial commit

---
+++ ConsoleWebApiHosting/Api/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SelfHostedApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello";
+        }
+
+    }
+}
---
+++ ConsoleWebApiHosting/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.Cors;
+namespace ConsoleWebApiHosting
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseCors(CorsOptions.AllowAll);
+            appBuilder.UseWebApi(config);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            WebApp.Start<Startup>(url: baseAddress);
+            Console.WriteLine("The Api Server is host is running now @ {0}. Press Ctrl + c to quit", baseAddress);
+            Console.ReadLine();
+        }
+    }
+}
